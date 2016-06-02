3dda6a3adafb4e3728c9939b4f4283b6c10003a3
CodeXtronics <e-trash@hotmail.fr>
2016-06-02 11:04:58 +0200

    Bon Entrée v0.01

--- MaintinfoBll/BonEntreeManager.cs
+++ MaintinfoBll/BonEntreeManager.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MaintinfoBo;
 
 namespace MaintinfoBll
 {
     class BonEntreeManager
     {
+        public static void CreerBonEntree(string refArt,int quantite)
+        {
+            Article art = ArticleManager.RechercheArticle(refArt);
+            BonEntree BonE = new BonEntree(art, quantite, DateTime.Now);
+            
+        }
     }
 }
