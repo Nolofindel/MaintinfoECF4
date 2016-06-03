a20f10ea3688e8ce46f8f5b80319dc20825c486d
Rad <radouanelotfi@gmail.com>
2016-06-03 09:19:03 +0200

    test

--- MaintinfoDal/ArticleDao.cs
+++ MaintinfoDal/ArticleDao.cs
@@ -22,6 +22,7 @@ namespace MaintinfoDal
             CmdGetBy = string.Empty;
             CmdGetAll = "GetAllProduit";
             CmdGetAllBy = "RecupererArticle";
+            //test
 
         }
 
